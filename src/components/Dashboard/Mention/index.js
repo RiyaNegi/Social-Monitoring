432021f3c27deb6f3a86271093ff3ec82ca3d976
Riya <riyanegi221b@gmail.com>
2021-05-19 20:46:06 +0530

    fix files exchange

--- src/components/Dashboard/Mention/index.js
+++ src/components/Dashboard/Mention/index.js
@@ -6,79 +6,79 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 const data = [
     {
         name: '1st Jan 2021',
-        uv: 4000,
+        total: 4000,
         pv: 2400,
         amt: 2400,
     },
     {
         name: '10th Jan 2021',
-        uv: 2000,
+        total: 2000,
         pv: 1398,
         amt: 2210,
     },
     {
         name: '20th Jan 2021',
-        uv: 900,
+        total: 900,
         pv: 9800,
         amt: 2290,
     },
     {
         name: '30th Jan 2021',
-        uv: 1280,
+        total: 1280,
         pv: 3908,
         amt: 2000,
     },
     {
         name: '10th Feb 2021',
-        uv: 1890,
+        total: 1890,
         pv: 4800,
         amt: 2181,
     },
     {
         name: '20th Feb 2021',
-        uv: 1000,
+        total: 1000,
         pv: 3800,
         amt: 2500,
     },
     {
         name: '30th Feb 2021',
-        uv: 3490,
+        total: 3490,
         pv: 4300,
         amt: 2100,
     },
     {
         name: '10th Mar 2021',
-        uv: 1000,
+        total: 1000,
         pv: 4300,
         amt: 2100,
     },
     {
         name: '20th Mar 2021',
-        uv: 2490,
+        total: 2490,
         pv: 4300,
         amt: 2100,
     },
     {
         name: '30th Mar 2021',
-        uv: 1400,
+        total: 1400,
         pv: 4300,
         amt: 2100,
     },
     {
         name: '10th Apr 2021',
-        uv: 3400,
+        total: 3400,
         pv: 1398,
         amt: 2210,
     },
     {
         name: '20th Apr 2021',
-        uv: 2500,
+        total: 2500,
         pv: 9800,
         amt: 2290,
     },
     {
         name: '30th Apr 2021',
-        uv: 1280,
+        total: 1280,
         pv: 3908,
         amt: 2000,
     },
@@ -110,7 +110,7 @@ const Index = () => {
                 }}
             >
                 <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <linearGradient id="colortotal" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#8cafeb" stopOpacity={0.8} />
                         <stop offset="95%" stopColor="#8cafeb" stopOpacity={0} />
                     </linearGradient>
@@ -119,7 +119,7 @@ const Index = () => {
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
-                <Area type="monotone" dataKey="uv" stroke="#8cafeb" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="total" stroke="#8cafeb" fillOpacity={1} fill="url(#colortotal)" />
             </AreaChart>
         </ResponsiveContainer>
     </div >
--- src/components/Dashboard/Predictor/index.js
+++ src/components/Dashboard/Predictor/index.js
@@ -4,83 +4,47 @@ import Tooltipp from 'react-bootstrap/Tooltip'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 const data = [
     {
-        name: '1st Jan 2021',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        name: '1st Jan',
+        low: 5,
+        high: 2,
+        amt: 2,
     },
     {
-        name: '10th Jan 2021',
-        uv: 2000,
-        pv: 1398,
-        amt: 2210,
+        name: '2nd Jan',
+        low: 2,
+        high: 5,
+        amt: 2,
     },
     {
-        name: '20th Jan 2021',
-        uv: 900,
-        pv: 9800,
-        amt: 2290,
+        name: '3rd Jan',
+        low: 9,
+        high: 7,
+        amt: 2,
     },
     {
-        name: '30th Jan 2021',
-        uv: 1280,
-        pv: 3908,
-        amt: 2000,
+        name: '4th Jan',
+        low: 1,
+        high: 3,
+        amt: 2,
     },
     {
-        name: '10th Feb 2021',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        name: '5th Jan',
+        low: 5,
+        high: 9,
+        amt: 2,
     },
     {
-        name: '20th Feb 2021',
-        uv: 1000,
-        pv: 3800,
-        amt: 2500,
+        name: '6th Jan',
+        low: 1,
+        high: 3,
+        amt: 2,
     },
     {
-        name: '30th Feb 2021',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: '10th Mar 2021',
-        uv: 1000,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: '20th Mar 2021',
-        uv: 2490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: '30th Mar 2021',
-        uv: 1400,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: '10th Apr 2021',
-        uv: 3400,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: '20th Apr 2021',
-        uv: 2500,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: '30th Apr 2021',
-        uv: 1280,
-        pv: 3908,
-        amt: 2000,
-    },
+        name: '7th Jan',
+        low: 5,
+        high: 4,
+        amt: 2,
+    }
 ];
 
 const Predictor = () => {
@@ -108,11 +72,11 @@ const Predictor = () => {
                 }}
             >
                 {/* <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <linearGradient id="colorlow" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#E25960" stopOpacity={0.8} />
                         <stop offset="95%" stopColor="#E25960" stopOpacity={0} />
                     </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <linearGradient id="colorhigh" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#323232" stopOpacity={0.8} />
                         <stop offset="95%" stopColor="#323232" stopOpacity={0} />
                     </linearGradient>
@@ -121,8 +85,8 @@ const Predictor = () => {
                 {/* <CartesianGrid strokeDasharray="5 5" /> */}
                 <YAxis />
                 <Tooltip />
-                <Area type="monotone" dataKey="uv" stroke="#627aa4" fill="#627aa4" />
-                <Area type="monotone" dataKey="pv" stroke="#eb8c9c" fill="#eb8c9c" />
+                <Area type="monotone" dataKey="low" stroke="#627aa4" fill="#627aa4" />
+                <Area type="monotone" dataKey="high" stroke="#eb8c9c" fill="#eb8c9c" />
             </AreaChart>
         </ResponsiveContainer>
     </div>
